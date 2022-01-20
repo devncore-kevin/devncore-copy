238f2053cecf2edd43ded12ed069713165b6a397
kevin.kim <kevin.kim@devncore.org>
2022-01-20 22:26:14 +0900

    Update

---
+++ docs/src/components/portal/TopMenu.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import { styled } from '@mui/material/styles';
+
+
+const menuBox = {
+    marginLeft: '0px',
+    bgcolor: 'transparent'
+}
+
+const TopToolbar = styled(Toolbar)({
+    marginTop: 0,
+    padding: '0px',
+});
+
+const TopBox = styled(Box)({
+    borderBottom: '1px solid #cccccc',
+    backgroundColor: '#fafafa',
+});
+
+export default function TopMenu(props) {
+    return (
+        <>
+        </>
+    );
+}
